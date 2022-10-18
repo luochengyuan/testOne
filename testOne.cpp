a0676e555c41c52f8af749d0b7ea9e4cddf759dc
luo <208660534@qq.com>
2022-10-18 17:49:37 +0800

    "测试远端"

--- testOne.cpp
+++ testOne.cpp
@@ -4,5 +4,6 @@ int main(int argc char* argv[])
 	std::cout<<"this is a change"<<std::endl;
 	std::cout<<"this is a test"<<std::endl;
 	std::cout<<"this is the thrid change"<<std::endl;
+	std::cout<<"SUCCESS"<<std::end;
 	return 0;
 }
